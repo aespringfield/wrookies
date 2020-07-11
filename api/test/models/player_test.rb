50dbb3d3c8f7e00a3d8e9733afdd9e2fb9d7db01
Anna Springfield <anna.springfield@annas-C02YC656JG5J.local>
2020-07-11 14:37:18 -0500

    Use docker-compose db, add fixtures

--- api/test/models/player_test.rb
+++ api/test/models/player_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "bloop" do
+    assert true
+  end
 end
